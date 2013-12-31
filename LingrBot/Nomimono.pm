dc92b0b076a47a5571dc91d7da246900f5ad63c0
rarere <rarere@gmail.com>
2013-12-31 13:17:37 +0900

    modified.

--- LingrBot/Nomimono.pm
+++ LingrBot/Nomimono.pm
@@ -4,7 +4,7 @@ use v5.14;
 use warnings;
 use utf8;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 sub get_text {
     my $class = shift;
@@ -14,10 +14,23 @@ sub get_text {
 
     my $nomimono_text = "";
     if ($text =~ /マスター、(.*)一杯/) {
-        $nomimono_text = "つ ".$1;
+        $nomimono_text = ippai($1);
+    } elsif ($text =~ /会計/) {
+        $nomimono_text = kaikei();
     }
 
     return $nomimono_text;
+}
+
+sub ippai {
+    my ($text) = @_;
+    my $ret = "つ ".$text;
+    return $ret;
+}
+
+sub kaikei {
+    my $ret = int(rand(10000)) . "円になります";
+    return $ret;
 }
 
 1;
--- t/05_nomimono.t
+++ t/05_nomimono.t
@@ -8,10 +8,17 @@ subtest 'test' => sub {
     my $str = Nomimono->get_text("!tekitou sac 12");
     is $str, '';
 };
+
 subtest 'otya' => sub {
     my $str = Nomimono->get_text("マスター、お茶一杯");
     is $str, 'つ お茶';
 };
+
+subtest 'kaikei' => sub {
+    my $str = Nomimono->get_text("マスタ、会計");
+    like $str, qr/\d+円になります/;
+};
+
 
 
 done_testing;
